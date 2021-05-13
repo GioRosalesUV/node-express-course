c254c955a8927b88dabb826442ec6d3da75fdd5c
Giovanni Rosales <grosales@unitedvirtualities.com>
2021-05-13 15:59:13 -0600

    Fixed users route and add dynamic GET route

--- server.js
+++ server.js
@@ -6,11 +6,20 @@ const mockUserData = [
   { name: "Gio" },
 ];
 
-app.get('/', (req, res) => {
+app.get('/users', (req, res) => {
   res.json({
     success: true,
     message: "successfully got users. Nice!!",
     users: mockUserData
+  });
+});
+
+app.get('/users/:id', (req, res) => {
+  console.log(req.params.id);
+  res.json({
+    success: true,
+    message: "got one user",
+    user: req.params.id
   });
 });
 
