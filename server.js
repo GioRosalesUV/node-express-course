e62610fb7d0a46b95b51bb594c36526779f5a8fa
Giovanni Rosales <grosales@unitedvirtualities.com>
2021-05-13 15:56:16 -0600

    Add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 
+const mockUserData = [
+  { name: "Daniel" },
+  { name: "Gio" },
+];
+
+app.get('/', (req, res) => {
+  res.json({
+    success: true,
+    message: "successfully got users. Nice!!",
+    users: mockUserData
+  });
+});
+
 app.listen(3000, () => {
   console.log("server is running");
 });
